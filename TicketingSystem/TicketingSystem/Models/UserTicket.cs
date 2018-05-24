15c65a56c622f6f3454d7e518ea8098043b36a8e
eman elzohairy <eman.elzohairy995@gmail.com>
2018-05-24 16:32:17 +0200

    models
    create models

---
+++ TicketingSystem/TicketingSystem/Models/UserTicket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace TicketingSystem.Models
+{
+    public class UserTicket
+    {
+        [Key]
+        public int user_id { get; set; }
+        [Key]
+        public int ticket_id{ get; set; }
+        public string status { get; set; }
+        public Ticket ticket { get; set; }
+        public ApplicationUser User{ get; set; }
+
+    }
+}
