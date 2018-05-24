7bbde29921a8450a74d5dc827152c1748f308339
yomna-hassan <36695085+yomna-hassan@users.noreply.github.com>
2018-05-24 16:33:09 +0200

    commit

---
+++ TicketingSystem/TicketingSystem/Models/Presence.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TicketingSystem.Models
+{
+    public class Presence
+    {
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public bool Status { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public ApplicationUser User { get; set; }
+
+
+    }
+}
