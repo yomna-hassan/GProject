917309ed6749299b56af7c17cac3eec384b68ceb
Anan Ali <eng.anan93@gmail.com>
2018-06-01 21:33:59 +0200

    3n3onaa

---
+++ TicketingSystem/TicketingSystem/Controllers/LayerController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TicketingSystem.Models;
+
+namespace TicketingSystem.Controllers
+{
+    public class LayerController : ApiController
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        List<Layer> Layers = new List<Layer>();
+        List<int> Layers_Id = new List<int>();
+        List<Layer_SLA> Layer_Sla = new List<Layer_SLA>();
+        public IHttpActionResult GetLayerBySla(int sla)
+        {
+            Layer_Sla = db.LayerSLAs.Where(n => n.SLAId == sla).ToList();
+            foreach(var s in Layer_Sla)
+            {
+                Layers_Id.Add(s.LayerId);
+            }
+            foreach(var la in Layers_Id)
+            {
+                Layers.Add(db.Layers.Where(l => l.Layer_id == la).FirstOrDefault());
+            }
+            if (Layers == null)
+                return NotFound();
+            else
+                return Ok(Layers);
+
+        }
+
+
+    }
+}
---
+++ TicketingSystem/TicketingSystem/Controllers/TechnicianController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TicketingSystem.Models;
+
+namespace TicketingSystem.Controllers
+{
+    public class TechnicianController : ApiController
+    {
+        List<ApplicationUser> Technicians = new List<ApplicationUser>();
+        ApplicationDbContext db = new ApplicationDbContext();
+        public IHttpActionResult GetTechByLayer(int LayerId)
+        {
+            Technicians = db.Users.Where(u => u.layer_id == LayerId).ToList();
+            if (Technicians == null)
+                return NotFound();
+            else
+                return Ok(Technicians);
+        }
+    }
+}
