594644a834d78bdf6aa1e4da2fa0100f867ea1a6
Anan Ali <eng.anan93@gmail.com>
2018-06-02 14:08:44 +0200

    ananfromhadeer

--- TicketingSystem/TicketingSystem/Migrations/201806020956473_m1.Designer.cs
+++ TicketingSystem/TicketingSystem/Migrations/201806020956473_m1.Designer.cs
@@ -7,13 +7,13 @@ namespace TicketingSystem.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class m5 : IMigrationMetadata
+    public sealed partial class m1 : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(m5));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(m1));
         
         string IMigrationMetadata.Id
         {
-            get { return "201806011828368_m5"; }
+            get { return "201806020956473_m1"; }
         }
         
         string IMigrationMetadata.Source
