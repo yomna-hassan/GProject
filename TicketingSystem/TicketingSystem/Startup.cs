c55a242f90b8a2c4951db9eedd25ca7d8eb1209e
yomna-hassan <36695085+yomna-hassan@users.noreply.github.com>
2018-06-07 01:15:35 +0200

    ya saatr

--- TicketingSystem/TicketingSystem/Startup.cs
+++ TicketingSystem/TicketingSystem/Startup.cs
@@ -6,7 +6,7 @@ using Owin;
 using TicketingSystem.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
-
+using Microsoft.Owin.Cors;
 
 [assembly: OwinStartup(typeof(TicketingSystem.Startup))]
 
@@ -55,44 +55,7 @@ namespace TicketingSystem
                 role.Name = "Technician";
                 rolemanager.Create(role);
 
-                var tech = new ApplicationUser();
-                tech.UserName = "Mohamed Kareem";
-                tech.Email = "Mohamed@gmail.com";
-                tech.layer_id = 1;
-
-                string userpassword2 = "Mohamed_11";
-                var checkuser2 = usermanager.Create(tech, userpassword2);
-
-                if (checkuser2.Succeeded)
-                {
-                    var result = usermanager.AddToRole(tech.Id, "Technician");
-                }
-
-                var tech2 = new ApplicationUser();
-                tech2.UserName = "Amir Ahmed";
-                tech2.Email = "Amir@gmail.com";
-                tech2.layer_id = 2;
-
-                string userpassword3 = "Amir_10";
-                var checkuser3 = usermanager.Create(tech2, userpassword3);
-
-                if (checkuser3.Succeeded)
-                {
-                    var result = usermanager.AddToRole(tech2.Id, "Technician");
-                }
-
-                var tech3 = new ApplicationUser();
-                tech2.UserName = "Waleed Seif";
-                tech2.Email = "Waleed@gmail.com";
-                tech2.layer_id = 3;
-
-                string userpassword4 = "Waleed_33";
-                var checkuser4 = usermanager.Create(tech3, userpassword4);
-
-                if (checkuser4.Succeeded)
-                {
-                    var result = usermanager.AddToRole(tech3.Id, "Technician");
-                }
+             
             }
            
            
@@ -103,7 +66,8 @@ namespace TicketingSystem
   
         public void Configuration(IAppBuilder app)
         {
-           
+            app.UseCors(CorsOptions.AllowAll);
+
             ConfigureAuth(app);
             CreateRoleAndUser();
             app.MapSignalR();
